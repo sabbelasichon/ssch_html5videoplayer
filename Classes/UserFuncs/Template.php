9c2251e8026ce595621cf1fbae60ff92014e2c40
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-07-05 23:44:27 +0200

    [BUGFIX] Use multiple template root paths in flexforms

--- Classes/UserFuncs/Template.php
+++ Classes/UserFuncs/Template.php
@@ -14,6 +14,9 @@ namespace Ssch\SschHtml5videoplayer\UserFuncs;
  *
  * The TYPO3 project - inspiring people to share!
  */
+
+use Exception;
+use function GuzzleHttp\Psr7\parse_query;
 use TYPO3\CMS\Core\Html\HtmlParser;
 use TYPO3\CMS\Core\TypoScript\ExtendedTemplateService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -22,14 +25,17 @@ use TYPO3\CMS\Frontend\Page\PageRepository;
 class Template
 {
     /**
-     * @param array  $params
+     * @param array $params
      * @param object $pObj
+     *
+     * @throws Exception
      */
     public function main(&$params, &$pObj)
     {
-        global $BE_USER;
+        $url = GeneralUtility::_GET('returnUrl');
+        $urlInfo = parse_query($url);
 
-        $thePageId = $params['row']['pid'];
+        $thePageId = $urlInfo['id']; //$params['row']['pid'];
         $template = GeneralUtility::makeInstance(ExtendedTemplateService::class);   // Defined global here!
         /* @var $template ExtendedTemplateService */
         $template->tt_track = 0;
@@ -41,60 +47,60 @@ class Template
         // This generates the constants/config + hierarchy info for the template.
         $template->runThroughTemplates($rootLine, 0);
         $template->generateConfig();
-
         $fullPath = false;
-        switch ($params['config']['path']) {
-            case 'JavaScripts':
-                $fullPath = true;
-                $extFileList = 'html,htm,js';
-                $pathToFiles = $template->setup['plugin.']['tx_sschhtml5videoplayer.']['view.']['templateRootPathInitializationFiles'];
-                break;
-            case 'Audio':
-                $extFileList = 'html,htm';
-                $pathToFiles = $template->setup['plugin.']['tx_sschhtml5videoplayer.']['view.']['templateRootPath'] . 'Audio/';
-                break;
-            default:
-                $extFileList = 'html,htm';
-                $pathToFiles = $template->setup['plugin.']['tx_sschhtml5videoplayer.']['view.']['templateRootPath'] . 'Video/';
-                break;
+
+        $type = $params['config']['path'] ?: 'Video';
+        $extFileList = 'html,htm';
+
+        if ($type === 'JavaScripts') {
+            $fullPath = true;
+            $extFileList = 'html,htm,js';
+            $templateRootPaths = (array)$template->setup['plugin.']['tx_sschhtml5videoplayer.']['view.']['templateRootPathInitializationFiles'];
+        } else {
+            $templateRootPaths = array_map(static function ($templateRootPath) use ($type) {
+                return $templateRootPath.$type;
+            }, $template->setup['plugin.']['tx_sschhtml5videoplayer.']['view.']['templateRootPaths.']);
         }
 
-        // Finding value for the path containing the template files
-        $readPath = GeneralUtility::getFileAbsFileName($pathToFiles);
+        foreach ($templateRootPaths as $templateRootPath) {
 
-        // If that direcotry is valid, is a directory then select files in it:
-        if (is_dir($readPath)) {
-            //getting all HTML files in the directory:
-            $templateFiles = GeneralUtility::getFilesInDir($readPath, $extFileList, 1, 1);
-            // Start up the HTML parser:
-            $parseHTML = GeneralUtility::makeInstance(HtmlParser::class);
-            /* @var $parserHTML \TYPO3\CMS\Core\Html\HtmlParser */
-            foreach ($templateFiles as $htmlFilePath) {
-                $selectorBoxItemIcon = '';
-                $content = GeneralUtility::getUrl($htmlFilePath);
-                $titles = $parseHTML->splitIntoBlock('titles', $content);
-                $language = $BE_USER->uc['lang'] == '' ? 'default' : $BE_USER->uc['lang'];
+            // Finding value for the path containing the template files
+            $readPath = GeneralUtility::getFileAbsFileName($templateRootPath);
 
-                $titlesLang = $parseHTML->splitIntoBlock($language, $titles[1]);
-                if (count($titlesLang) < 2) {
-                    $titlesLang = $parseHTML->splitIntoBlock('default', $titles[1]);
-                }
-                $titleTagContent = $parseHTML->removeFirstAndLastTag($titlesLang[1]);
+            // If that direcotry is valid, is a directory then select files in it:
+            if (is_dir($readPath)) {
+                //getting all HTML files in the directory:
+                $templateFiles = GeneralUtility::getFilesInDir($readPath, $extFileList, 1, 1);
+                // Start up the HTML parser:
+                $parseHTML = GeneralUtility::makeInstance(HtmlParser::class);
+                /* @var $parserHTML \TYPO3\CMS\Core\Html\HtmlParser */
+                foreach ($templateFiles as $htmlFilePath) {
+                    $selectorBoxItemIcon = '';
+                    $content = GeneralUtility::getUrl($htmlFilePath);
+                    $titles = $parseHTML->splitIntoBlock('titles', $content);
+                    $language = $GLOBALS['BE_USER']->uc['lang'] === '' ? 'default' : $GLOBALS['BE_USER']->uc['lang'];
 
-                if ($titleTagContent) {
-                    $selectorBoxItemTitle = trim($titleTagContent . ' (' . basename($htmlFilePath) . ')');
-                    $fI = GeneralUtility::split_fileref($htmlFilePath);
-                    $testImageFilename = $readPath . 'Icons/' . $fI['filebody'] . '.gif';
-                    if (is_file($testImageFilename)) {
-                        $selectorBoxItemIcon = '../' . substr($testImageFilename, strlen(PATH_site));
+                    $titlesLang = $parseHTML->splitIntoBlock($language, $titles[1]);
+                    if (count($titlesLang) < 2) {
+                        $titlesLang = $parseHTML->splitIntoBlock('default', $titles[1]);
                     }
+                    $titleTagContent = $parseHTML->removeFirstAndLastTag($titlesLang[1]);
 
-                    if (true === $fullPath) {
-                        $pathToFile = $htmlFilePath;
-                    } else {
-                        $pathToFile = basename($htmlFilePath);
+                    if ($titleTagContent) {
+                        $selectorBoxItemTitle = trim($titleTagContent.' ('.basename($htmlFilePath).')');
+                        $fI = GeneralUtility::split_fileref($htmlFilePath);
+                        $testImageFilename = $readPath.'Icons/'.$fI['filebody'].'.gif';
+                        if (is_file($testImageFilename)) {
+                            $selectorBoxItemIcon = '../'.substr($testImageFilename, strlen(PATH_site));
+                        }
+
+                        if (true === $fullPath) {
+                            $pathToFile = $htmlFilePath;
+                        } else {
+                            $pathToFile = basename($htmlFilePath);
+                        }
+                        $params['items'][] = [$selectorBoxItemTitle, $pathToFile, $selectorBoxItemIcon];
                     }
-                    $params['items'][] = [$selectorBoxItemTitle, $pathToFile, $selectorBoxItemIcon];
                 }
             }
         }
